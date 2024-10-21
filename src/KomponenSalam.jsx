0588ac3ecc65d34967c2cc2e68f8f20141a64526
Andika Agung <atriadha@gmail.com>
2024-10-21 16:22:49 +0700

    Starting up

---
+++ src/KomponenSalam.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function KomponenSalam() {
+  return (
+    <div>
+      <h2>Selamat datang di aplikasi kami!</h2>
+    </div>
+  );
+}
+
+export default KomponenSalam;
